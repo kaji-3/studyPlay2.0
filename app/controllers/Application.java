c206ab1edd1bdcddc8815956a74328a94d0cd340
kajikawa <kajikawa@ms.scsk.jp>
2012-03-29 12:39:53 +0900

    Action compositionのAuthenticated、Cachedのための準備。

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,16 @@
 package controllers;
 
 import play.*;
+import play.cache.Cached;
 import play.mvc.*;
 import play.mvc.Http.RequestBody;
+import play.mvc.Security.Authenticated;
 
 import views.html.*;
 
 public class Application extends Controller {
-
 	@With(VerboseAction.class)
-	public static Result index() {
+	public static Result study() {
 	    Logger.info("Calling action for2 ");
 	  return ok("It works!");
 	}
